d46475a01985862a5f8032731d464c7a512305fa
bessyjl <631908942@qq.com>
2019-09-01 00:10:26 +0800

    Site updated: 2019-09-01 00:10:23

--- dist/music.js
+++ dist/music.js
@@ -3,6 +3,12 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
+	  {
+        name: 'Señorita',
+        artist: 'Shawn Mendes _ Camila Cabello',
+        url: 'http://qiniu.bbsky.xyz/music/Shawn%20Mendes%20_%20Camila%20Cabello%20-%20Se%C3%B1orita.mp3',
+        cover: 'http://qiniu.bbsky.xyz/img_music/Se%C3%B1orita.png',
+      },
       {
         name: "黑暗森林",
         artist: '雲翼星辰',
@@ -14,6 +20,12 @@ const ap = new APlayer({
         artist: 'Matthew Perryman Jones',
         url: 'http://qiniu.bbsky.xyz/music/Matthew%20Perryman%20Jones%20-%20Living%20in%20the%20Shadows.mp3',
         cover: 'http://qiniu.bbsky.xyz/img_music/Living%20in%20the%20Shadows.png',
+      },
+	  {
+        name: '来自天堂的魔鬼',
+        artist: 'G.E.M. 邓紫棋',
+        url: 'http://qiniu.bbsky.xyz/music/G.E.M.%20%E9%82%93%E7%B4%AB%E6%A3%8B%20-%20%E6%9D%A5%E8%87%AA%E5%A4%A9%E5%A0%82%E7%9A%84%E9%AD%94%E9%AC%BC.mp3',
+        cover: 'http://qiniu.bbsky.xyz/img_music/%E6%9D%A5%E8%87%AA%E5%A4%A9%E5%A0%82%E7%9A%84%E9%AD%94%E9%AC%BC.png',
       }
     ]
 });
---
+++ dist/video_about_2.js
@@ -0,0 +1,6 @@
+const dp = new DPlayer({
+    container: document.getElementById('dplayer'),
+    video: {
+        url: 'http://qiniu.bbsky.xyz/video/Dream%20Crazy.mp4'
+    },
+});
