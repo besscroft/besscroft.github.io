48834126cd3c3278f4befa29a6845414845deafc
bessyjl <631908942@qq.com>
2019-09-18 13:18:54 +0800

    Site updated: 2019-09-18 13:18:53

--- dist/music.js
+++ dist/music.js
@@ -6,44 +6,44 @@ const ap = new APlayer({
 	  {
         name: 'Señorita',
         artist: 'Shawn Mendes _ Camila Cabello',
-        url: 'http://pxyel3bmy.bkt.clouddn.com/music/Shawn%20Mendes%20_%20Camila%20Cabello%20-%20Se%C3%B1orita.mp3',
-        cover: 'http://pxyel3bmy.bkt.clouddn.com/img_music/Se%C3%B1orita.png',
+        url: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/music/Shawn Mendes _ Camila Cabello - Señorita.mp3',
+        cover: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/img/img_music/Señorita.png',
       },
 	  {
         name: 'I Shall Rise (Original Game Soundtrack)',
         artist: 'Karen O',
         url: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/music/I Shall Rise (Original Game Soundtrack) - Karen O.mp3',
-        cover: 'http://pxyel3bmy.bkt.clouddn.com/img_music/I%20Shall%20Rise.png',
+        cover: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/img/img_music/I Shall Rise.png',
       },
 	  {
         name: 'Boom Clap',
         artist: 'Charli XCX',
-        url: 'http://pxyel3bmy.bkt.clouddn.com/music/Boom_Clap-Charli_XCX.flac',
-        cover: 'http://pxyel3bmy.bkt.clouddn.com/img_music/Boom_Clap.png',
+        url: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/music/Boom_Clap-Charli_XCX.flac',
+        cover: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/img/img_music/Boom_Clap.png',
       },
 	  {
         name: 'Obsession',
         artist: 'Vice Jon Bellion',
-        url: 'http://pxyel3bmy.bkt.clouddn.com/music/Obsession-Vice_Jon_Bellion.mp3',
-        cover: 'http://pxyel3bmy.bkt.clouddn.com/img_music/Obsession.png',
+        url: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/music/Obsession-Vice_Jon_Bellion.mp3',
+        cover: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/img/img_music/Obsession.png',
       },
       {
         name: "黑暗森林",
         artist: '雲翼星辰',
-        url: 'http://pxyel3bmy.bkt.clouddn.com/music/%E9%9B%B2%E7%BF%BC%E6%98%9F%E8%BE%B0%20-%20%E9%BB%91%E6%9A%97%E6%A3%AE%E6%9E%97%EF%BC%88%E5%8E%9F%E6%9B%B2%EF%BC%9ASchwarzer%20Regen%EF%BC%89.mp3',
-        cover: 'http://pxyel3bmy.bkt.clouddn.com/img_music/%E9%BB%91%E6%9A%97%E6%A3%AE%E6%9E%97.png',
+        url: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/music/雲翼星辰 - 黑暗森林（原曲：Schwarzer Regen）.mp3',
+        cover: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/img/img_music/黑暗森林.png',
       },
       {
         name: 'Living in the Shadows',
         artist: 'Matthew Perryman Jones',
-        url: 'http://pxyel3bmy.bkt.clouddn.com/music/Matthew%20Perryman%20Jones%20-%20Living%20in%20the%20Shadows.mp3',
-        cover: 'http://pxyel3bmy.bkt.clouddn.com/img_music/Living%20in%20the%20Shadows.png',
+        url: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/music/Matthew Perryman Jones - Living in the Shadows.mp3',
+        cover: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/img/img_music/Living in the Shadows.png',
       },
 	  {
         name: '来自天堂的魔鬼',
         artist: 'G.E.M. 邓紫棋',
-        url: 'http://pxyel3bmy.bkt.clouddn.com/music/G.E.M.%20%E9%82%93%E7%B4%AB%E6%A3%8B%20-%20%E6%9D%A5%E8%87%AA%E5%A4%A9%E5%A0%82%E7%9A%84%E9%AD%94%E9%AC%BC.mp3',
-        cover: 'http://pxyel3bmy.bkt.clouddn.com/img_music/%E6%9D%A5%E8%87%AA%E5%A4%A9%E5%A0%82%E7%9A%84%E9%AD%94%E9%AC%BC.png',
+        url: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/music/G.E.M. 邓紫棋 - 来自天堂的魔鬼.mp3',
+        cover: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/img/img_music/来自天堂的魔鬼.png',
       }
     ]
 });
