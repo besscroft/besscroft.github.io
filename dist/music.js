f8aa294be664528a20255b0209ed28bdbe902d54
bessyjl <631908942@qq.com>
2019-10-04 21:08:22 +0800

    Site updated: 2019-10-04 21:08:18

--- dist/music.js
+++ dist/music.js
@@ -4,12 +4,6 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
 	  {
-        name: 'Señorita',
-        artist: 'Shawn Mendes _ Camila Cabello',
-        url: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/music/Shawn Mendes _ Camila Cabello - Señorita.mp3',
-        cover: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/img/img_music/Señorita.png',
-      },
-	  {
         name: 'I Shall Rise (Original Game Soundtrack)',
         artist: 'Karen O',
         url: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/music/I Shall Rise (Original Game Soundtrack) - Karen O.mp3',
@@ -28,16 +22,48 @@ const ap = new APlayer({
         cover: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/img/img_music/Obsession.png',
       },
       {
-        name: "黑暗森林",
-        artist: '雲翼星辰',
-        url: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/music/雲翼星辰 - 黑暗森林（原曲：Schwarzer Regen）.mp3',
-        cover: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/img/img_music/黑暗森林.png',
-      },
-      {
         name: 'Living in the Shadows',
         artist: 'Matthew Perryman Jones',
         url: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/music/Matthew Perryman Jones - Living in the Shadows.mp3',
         cover: 'https://cdn.jsdelivr.net/gh/bessyjl/cdn@1.3/img/img_music/Living in the Shadows.png',
+      },
+	  {
+        name: 'Señorita',
+        artist: 'Shawn Mendes _ Camila Cabello',
+        url: 'http://music.163.com/song/media/outer/url?id=1373168742.mp3',
+        cover: 'http://pxyel3bmy.bkt.clouddn.com/img_music/Se%C3%B1orita.png',
+      },
+	  {
+        name: "黑暗森林",
+        artist: '雲翼星辰',
+        url: 'http://music.163.com/song/media/outer/url?id=416892104.mp3',
+        cover: 'http://pxyel3bmy.bkt.clouddn.com/img_music/%E9%BB%91%E6%9A%97%E6%A3%AE%E6%9E%97.png',
+      },{
+        name: "来自天堂的魔鬼",
+        artist: 'G.E.M.邓紫棋',
+        url: 'http://music.163.com/song/media/outer/url?id=36270426.mp3',
+        cover: 'http://pxyel3bmy.bkt.clouddn.com/img_music/%E6%9D%A5%E8%87%AA%E5%A4%A9%E5%A0%82%E7%9A%84%E9%AD%94%E9%AC%BC.png',
+      },{
+        name: "Say You Love Me",
+        artist: 'Seeb,Skylar Grey',
+        url: 'http://music.163.com/song/media/outer/url?id=554169472.mp3',
+        cover: 'http://pxyel3bmy.bkt.clouddn.com/img_music/Say%20You%20Love%20Me.png',
+      },{
+        name: "One Day",
+        artist: 'MatisYahu/Akon',
+        url: 'http://music.163.com/song/media/outer/url?id=21157332.mp3',
+        cover: 'http://pxyel3bmy.bkt.clouddn.com/img_music/oneday.png',
+      },{
+        name: "Vacation",
+        artist: 'The Go-Gos',
+        url: 'http://music.163.com/song/media/outer/url?id=3583950.mp3',
+        cover: 'http://pxyel3bmy.bkt.clouddn.com/img_music/vacation.png',
+      },
+	  {
+        name: "Vacation",
+        artist: 'Skylar Grey',
+        url: 'http://music.163.com/song/media/outer/url?id=1331322046.mp3',
+        cover: 'http://pxyel3bmy.bkt.clouddn.com/img_music/Everything%20I%20Need.png',
       }
     ]
 });
