32e8c2a83b5d73b62e2ad8526287cd74446e74f3
bessyjl <631908942@qq.com>
2019-10-14 17:21:27 +0800

    Site updated: 2019-10-14 17:21:26

--- dist/video.js
+++ dist/video.js
@@ -1,6 +1,6 @@
 const dp = new DPlayer({
     container: document.getElementById('dplayer'),
     video: {
-        url: 'https://onedrive.52bess.com/movie/miku.mp4'
+        url: 'https://onedrive.52bess.com/blog/video/I%20Vow%20To%20Thee%20My%20Country.mp4'
     },
 });
